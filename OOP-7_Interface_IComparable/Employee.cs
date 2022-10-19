4b569b187f4ba6038a223e88e16c98964c37ace9
fatihmaras <fatihmmaras@gmail.com>
2022-10-20 01:08:09 +0300

    Added IComparable structure

---
+++ OOP-7_Interface_IComparable/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OOP_7_Interface_IComparable
+{
+    public class Employee : IComparable
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public decimal Salary { get; set; }
+
+        public int CompareTo(object obj)   //Implemented edildi 
+        {
+            var other = (Employee)obj;    // CompareTo  methodundaki obejcti Employee olarak cast işlemi uyguladık.
+
+            if (this.Salary < other.Salary)
+            {
+                return -1;
+            }
+            else if (this.Salary.Equals(other.Salary))
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+
+            }
+        }
+        public override string ToString()
+        {
+            return $"{Id,-5}{FullName,-25}{Salary,-7}";
+
+        }
+    }
+}
---
+++ OOP-7_Interface_IComparable/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OOP_7_Interface_IComparable
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var employees = new List<Employee>()
+            {
+                new Employee()
+                {
+                    Id =1,
+                    FullName= "Ahmet Aslan",
+                    Salary=9500
+                },
+                 new Employee()
+                {
+                    Id =2,
+                    FullName= "Aslan Yıldırım",
+                    Salary=13250
+                },
+                  new Employee()
+                {
+                    Id =3,
+                    FullName= "Merve Dağ",
+                    Salary=4250
+                },
+                   new Employee()
+                {
+                    Id =4,
+                    FullName= "Züleyha Çan",
+                    Salary=7500
+                },
+                    new Employee()
+                {
+                    Id =5,
+                    FullName= "Çakır Büyüküstün",
+                    Salary=12000
+                },
+                     new Employee()
+                {
+                    Id =6,
+                    FullName= "Birol Baş",
+                    Salary=3500
+                },
+
+            };
+
+            employees.Sort();
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
+
